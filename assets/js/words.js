aa166a2a2e71e83986dd059f2528c3981f634034
William Dias <smdwilliam89@gmail.com>
2023-12-08 23:30:44 +0000

    feat(words.js | index.html): Change the words to countries instead and change the instructions text.

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,23 +1,43 @@
 // Words array
 export const words = [
   {
-    name: 'mouse',
-    hint: 'Allows you to interact with the digital world by moving and clicking.',
+    name: 'usa',
+    hint: 'Land of the free and the home of the brave',
   },
   {
-    name: 'keyboard',
-    hint: 'Input device that consists of a set of keys, each with a specific function.',
+    name: 'japan',
+    hint: 'Cherry blossoms and sushi.',
   },
   {
-    name: 'monitor',
-    hint: 'Computer peripheral that displays visual information, making it possible for users to see images and videos.',
+    name: 'brazil',
+    hint: 'Home to the Amazon rainforest and Carnival.',
   },
   {
-    name: 'bluetooth',
-    hint: 'Wireless technology, named after a medieval king known for uniting people.',
+    name: 'australia',
+    hint: 'The land Down Under with kangaroos and the Great Barrier Reef.',
   },
   {
-    name: 'software',
-    hint: 'These digital instructions and programs tell your computer or device how to perform tasks.',
+    name: 'india',
+    hint: 'Land of spices, the Taj-Mahal, and diverse cultures.',
+  },
+  {
+    name: 'canada',
+    hint: 'Home of maple syrup, hockey, and polite people.',
+  },
+  {
+    name: 'russia',
+    hint: 'The largest country in the world, famous for its ballet and Matryoshka dolls.',
+  },
+  {
+    name: 'mexico',
+    hint: 'Land of tacos, mariachi, and ancient civilizations like the Aztecs.',
+  },
+  {
+    name: 'china',
+    hint: 'Home to the Great Wall, pandas, and rich cultural heritage.',
+  },
+  {
+    name: 'germany',
+    hint: 'Known for sausages, beer, and the Autobahn.',
   },
 ];
