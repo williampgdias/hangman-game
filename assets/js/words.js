057f82553bdea76a2efed2aa94bf71e52cfaa917
William Dias <smdwilliam89@gmail.com>
2023-12-08 11:03:56 +0000

    fix(words.js): Separate the words in a external file

---
+++ assets/js/words.js
@@ -0,0 +1,23 @@
+// Words array
+export const words = [
+  {
+    name: 'mouse',
+    hint: 'Allows you to interact with the digital world by moving and clicking.',
+  },
+  {
+    name: 'keyboard',
+    hint: 'Input device that consists of a set of keys, each with a specific function.',
+  },
+  {
+    name: 'monitor',
+    hint: 'Computer peripheral that displays visual information, making it possible for users to see images and videos.',
+  },
+  {
+    name: 'bluetooth',
+    hint: 'Wireless technology, named after a medieval king known for uniting people.',
+  },
+  {
+    name: 'software',
+    hint: 'These digital instructions and programs tell your computer or device how to perform tasks.',
+  },
+];
